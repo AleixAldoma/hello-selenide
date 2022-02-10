6abd1e3d98d3af9fadd07481fd59dc9e7e194ea7
Aleix Aldoma <aleixaldomasoto@gmail.com>
2022-02-10 09:49:43 +0100

    Added Cucumber tests.

--- build/resources/test/com/sinensia/helloselenide/cucumber/robobar.feature
+++ build/resources/test/com/sinensia/helloselenide/cucumber/robobar.feature
@@ -9,3 +9,48 @@ Feature: Robobar cart
     When user adds a cola
     And user adds a cola
     Then total should be €2.50
+
+  Scenario: user add one beer
+    Given user opens Robobar website
+    When user adds a beer
+    Then total should be €2.00
+
+  Scenario: user add two beers
+    Given user opens Robobar website
+    When user adds a beer
+    And user adds a beer
+    Then total should be €4.00
+
+  Scenario: user add one wine
+    Given user opens Robobar website
+    When user adds a wine
+    Then total should be €3.00
+
+  Scenario: user add two wines
+    Given user opens Robobar website
+    When user adds a wine
+    And user adds a wine
+    Then total should be €6.00
+
+  Scenario: underage buys cola
+    Given user opens Robobar website
+    When user adds a cola
+    And user checkout
+    And user order
+    Then message should be "Coming right up! ~bzzzt~"
+
+  Scenario: underage buys beer
+    Given user opens Robobar website
+    When user adds a beer
+    And user checkout
+    And user inputs age "17"
+    And user order
+    Then alert should appear
+
+  Scenario: adult buys beer
+    Given user opens Robobar website
+    When user adds a beer
+    And user checkout
+    And user inputs age "19"
+    And user order
+    Then message should be "Coming right up! ~bzzzt~"
--- src/test/java/com/sinensia/helloselenide/cucumber/RobobarStepDefinitions.java
+++ src/test/java/com/sinensia/helloselenide/cucumber/RobobarStepDefinitions.java
@@ -1,20 +1,28 @@
 package com.sinensia.helloselenide.cucumber;
 
 import com.sinensia.helloselenide.CartPage;
+import com.sinensia.helloselenide.CheckoutPage;
+import com.sinensia.helloselenide.OrderPage;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import static com.codeborne.selenide.Condition.hidden;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.open;
 
 public class RobobarStepDefinitions {
     CartPage cartPage;
+    CheckoutPage checkoutPage;
+    OrderPage orderPage;
 
     @Given("user opens Robobar website")
     public void userOpensRobobarWebsite() {
         open("http://localhost:3000");
         cartPage = new CartPage();
+        checkoutPage = new CheckoutPage();
+        orderPage = new OrderPage();
     }
 
     @When("user adds a cola")
@@ -25,5 +33,40 @@ public class RobobarStepDefinitions {
     @Then("total should be €{double}")
     public void totalShouldBe€(Double total) {
         cartPage.total().shouldBe(text(total.toString()));
+    }
+
+    @When("user adds a beer")
+    public void userAddsABeer() {
+        cartPage.addBeer();
+    }
+
+    @When("user adds a wine")
+    public void userAddsAWine() {
+        cartPage.addWine();
+    }
+
+    @And("user checkout")
+    public void userCheckout() {
+        cartPage.checkout();
+    }
+
+    @And("user inputs age {string}")
+    public void userInputsAge(String age) {
+        checkoutPage.setAge(age);
+    }
+
+    @And("user order")
+    public void userOrder() {
+        checkoutPage.order();
+    }
+
+    @Then("message should be {string}")
+    public void messageShouldBeComingRightUp(String msg) {
+        orderPage.getSentMessage().shouldBe(text(msg));
+    }
+
+    @Then("alert should appear")
+    public void alertShouldAppear() {
+        orderPage.getAlertDiv().shouldNotBe(hidden);
     }
 }
